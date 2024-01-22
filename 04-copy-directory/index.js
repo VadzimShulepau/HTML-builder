c4555ba46fffcf0284514b534c1e36790ce44480
VadzimShulepau <shulepau.vadzim@gmail.com>
2024-01-22 21:47:57 +0300

    feat: task 04-copy-directory is done

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const filesFolder = path.resolve(__dirname, 'files');
+const filesCopyFolder = path.resolve(__dirname, 'files-copy');
+
+fs.access(filesCopyFolder, async (err) => {
+  if (err) console.log(err);
+
+  await fs.promises.rm(filesCopyFolder, { recursive: true, force: true });
+  await fs.promises.mkdir(filesCopyFolder, { recursive: true });
+
+  const files = await fs.promises.readdir(filesFolder, { writingMode: true });
+  for (let file of files) {
+    fs.copyFile(
+      path.resolve(filesFolder, file),
+      path.resolve(filesCopyFolder, file),
+      (err) => {
+        if (err) return new Error('coping failed');
+      },
+    );
+  }
+});
